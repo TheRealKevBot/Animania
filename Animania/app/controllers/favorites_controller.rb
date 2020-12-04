3d52d67dad932b49c30b9f404cf982058aae9930
TheRealKevBot <Kglas001@gmail.com>
2020-12-04 11:48:39 -0700

    Backend functions Sign up form start

--- Animania/app/controllers/favorites_controller.rb
+++ Animania/app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+
+    def index
+        @favorites = Favorite.all
+
+        render json: @favorites
+    end
+
+    def show
+        @favorite = Favorite.find(params[:id])
+
+        render json: @favorite
+    end
+
 end
