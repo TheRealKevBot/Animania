bb3b9ba676e5f222e8a2b21cb9210955ec43c4cc
jay719 <javariab17@gmail.com>
2020-12-07 01:44:40 -0700

    refactored code

--- Animania-app/sign-in-screen/index.js
+++ Animania-app/sign-in-screen/index.js
@@ -2,6 +2,8 @@ top50AnimeURL = 'https://api.jikan.moe/v3/top/anime';
 allAnimeRecentlyReleasedURL = 'https://api.jikan.moe/v3/search/anime?q=&page=1&genre=1,10&order_by=start_date&sort=desc';
 userURL = "http://localhost:3000/users"
 const top50anime = document.querySelector('section')
+const sideCard = document.querySelector('side-bar2')
+
 
 const signIn = document.getElementById('btn')
 const modal = document.getElementById('myModal')
@@ -10,7 +12,7 @@ const closer = document.getElementsByClassName('close')[0]
 
 
 fetch(top50AnimeURL)
-    .then(parseJSON
+    .then(parseJSON)
     .then(displayTop50);
 
 function displayTop50(animes) {
@@ -18,24 +20,50 @@ function displayTop50(animes) {
 }
 
 function showAnime(anime) {
-    const animeCard = document.createElement('div')
-    animeCard.classList.add("cardR")
-    
-    const ranking = document.createElement('h2')
-    ranking.textContent = anime.rank; 
+   const animeCard = createAnimeCard();
+   const name = createAnimeName(anime);
+   const ranking = createAnimeRanking(anime);
+   const cover = createImage(anime);
+   const  synopsis = createAnimeSynop(anime);
 
-    const name = document.createElement('h3')
-    name.textContent = anime.title;
-
-    const cover = document.createElement('img')
-    cover.src = anime.image_url;
-    cover.alt = anime.title; 
     
     animeCard.append(ranking, name, cover)
     top50anime.append(animeCard)
+    sideCard.append(synopsis)
     // document.body.append(top50anime)
 } 
 
+function createAnimeCard() { 
+  const animeCard=  document.createElement('div');
+  animeCard.classList.add("cardR");
+  return animeCard;
+} 
+
+function createAnimeRanking({ rank }) {
+  const ranking = document.createElement('h2');
+  ranking.textContent = rank; 
+  return ranking
+}
+function createAnimeName ( { title }){
+  const name = document.createElement('h3');
+  name.textContent = title;
+  return name
+}
+
+function createImage({image_url, title}) {
+  const cover = document.createElement('img')
+    cover.src = image_url;
+    cover.alt = title; 
+    return cover
+    
+}
+
+function createAnimeSynop ({synopsis}) {
+  const synop = document.createElement('p');
+  synop.classList.add('synopsis');
+  synop.textContent = synopsis;
+  return synop
+}
 
     // When the user clicks on the button, open the modal
     signIn.onclick = function() {
@@ -54,11 +82,7 @@ function showAnime(anime) {
         }
       }
 
-    fetch(userURL)
-      .then(response => response.json)
-      .then (accessUsers);
-
-
+    
   
 
      function  parseJSON(response) {
