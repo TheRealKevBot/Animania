ae3b9ea464501d982364b560610c969db58c48ae
TheRealKevBot <Kglas001@gmail.com>
2020-12-07 13:04:27 -0700

    Back and Front end integration

--- Animania-app/sign-in-screen/index.js
+++ Animania-app/sign-in-screen/index.js
@@ -10,7 +10,7 @@ const modal = document.getElementById('myModal')
 const closer = document.getElementsByClassName('close')[0]
 
 fetch(top50AnimeURL)
-    .then(response => response.json())
+    .then(parseJSON)
     .then(displayTop50);
 
 function displayTop50(animes) {
@@ -36,30 +36,19 @@ function showAnime(anime) {
     // document.body.append(top50anime)
 } 
 
+fetch(userURL)
+  .then(parseJSON)
+  .then (accessUsers);
+
+function  parseJSON(response) {
+  return response.json();
+}
+
 // When the user clicks on the button, open the modal
 signIn.onclick = function() {modal.style.display = "block";}
-      
+
 // When the user clicks on <span> (x), close the modal
 closer.onclick = function() {modal.style.display = "none";}
-      
-
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
-
-    fetch(userURL)
-      .then(response => response.json)
-      .then (accessUsers);
-
-
-  
-
-     function  parseJSON(response) {
-       return response.json();
-     }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -67,6 +56,7 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
 
 
 
--- Animania/db/seeds.rb
+++ Animania/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+response = RestClient.get ("https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1")
+result = JSON.parse response
+api_animes = result['results']
+
+
+api_animes.each do |api_anime|
+    puts api_anime['title']
+    @newAnime = Favorite.create(
+        name: api_anime['title'],
+        user_rating: api_anime['score']
+    )
+end
